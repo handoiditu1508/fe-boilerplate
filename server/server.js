f7f32ef32bd09783ac135eb65778761e273e87ec
Danh <handoiditu1508@gmail.com>
2022-06-08 11:40:46 +0700

    update file upload and get apis

--- server/server.js
+++ server/server.js
@@ -1,42 +1,101 @@
 const jsonServer = require("json-server");
-const server = jsonServer.create();
-const path = require('path');
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const app = jsonServer.create();
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const router = jsonServer.router(path.join(__dirname, "db.json"))
 const middlewares = jsonServer.defaults();
 const delayMiddleware = require("./middlewares/delay-middleware");
 const port = 3000;
+const delayMiliseconds = 2000;
+const upload = multer();
+const jwtSecret = "0123456789qwertyuiopasdfghjklzxcvbnm";
 
 // Add middlewares
-server.use(middlewares);
-server.use(delayMiddleware(2000));
+app.use(middlewares);
+app.use(delayMiddleware(delayMiliseconds));
 
 // To handle POST, PUT and PATCH you need to use a body-parser
-server.use(jsonServer.bodyParser);
+app.use(jsonServer.bodyParser);
 
 // Add custom routes before JSON Server router
-server.get("/api/echo", (req, res) => {
-  res.jsonp("hello!");
+app.get("/api/echo", (req, res) => {
+  res.json("hello!");
 });
 
-server.post("/api/echo", (req, res) => {
-  res.status(200).jsonp("added!");
+app.post("/api/echo", (req, res) => {
+  res.status(200).json("added!");
 });
 
 // Define a protected route
-server.get("/api/protected-echo", (req, res) => {
+app.get("/api/protected-echo", (req, res) => {
   if (isAuthorized(req)) {
-    res.jsonp("hello!");
+    res.json("hello!");
   } else {
     res.sendStatus(401);
   }
 });
 
-server.use("/api", router);
+app.get("/file/*", (req, res) => {
+  const filePath = path.join(__dirname, "public", req.params[0]);
+  res.sendFile(filePath, err => {
+    if (err) {
+      res.sendStatus(404);
+    }
+  });
+});
 
-server.listen(port, () => {
+app.post("/file", upload.single("file"), (req, res) => {
+  const destination = path.join(__dirname, "public", req.body.path);
+  const fileName = req.body.name;
+
+  fs.mkdir(destination, { recursive: true }, (err, createdPath) => {
+    if (err) {
+      console.log(err);
+    } else {
+      fs.writeFile(path.join(destination, fileName), req.file.buffer, err => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+  });
+
+  res.status(200).json({ success: true });
+});
+
+app.use("/api", router);
+
+app.listen(port, () => {
   console.log("JSON Server is running on http://localhost:" + port + ". Press Ctrl-C to terminate.");
 });
 
 function isAuthorized(req) {
+  const bearerHeader = req.headers["Authorization"];
+  if (bearerHeader != undefined) {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    return true;
+  }
   return false;
+}
+
+function createBearerToken(callback) {
+  const payload = {
+    id: Date.now(),
+    userName: "admin1",
+    email: "admin1@gmail.com"
+  };
+
+  jwt.sign(payload, jwtSecret, {
+    algorithm: "ES256",
+    audience: [`http://localhost:${port}`],
+    expiresIn: "1d",
+    issuer: `http://localhost:${port}`,
+    jwtid: crypto.randomUUID()
+  }, (err, encoded) => {
+    callback(err, encoded);
+  })
 }
